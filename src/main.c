9e0a396064425c8185896b7011f14846e2d808c2
João <jpedrofa15@gmail.com>
2020-10-15 21:28:15 -0300

    adicionada função para mover peças do quebra cabeças

--- src/main.c
+++ src/main.c
@@ -5,30 +5,32 @@ const int screenHeight = 480;
 
 Texture2D player_asset;
 Texture2D tile;
-
-Texture2D mascara;//mudei aqui
+Texture2D mascara;
 Texture2D virado;
 Rectangle teste = {200, 100, 60, 60};
 Rectangle grid = {70, 35, 260, 390};
 Rectangle box = {450, 35, 150, 390};
 Texture2D pieces[6];
-Rectangle piecesCoords[6];
+Rectangle coords[6];
+
 struct Vector2 mousePos = {0};
+
 int game_running = 1;
 
-bool checkMouseCollision();
 bool init(void);
 bool loadAssets(void);
 void update(void);
 void inputHandler(void);
 void close(void);
-void quebraCabeca(void);
-void checkPieces(Rectangle grid, Texture2D piece[]);
+void puzzle(void);
+void checkPieces();
+void checkMouse(void);
+void initPieces(void);
 
 int main() {
     if(!init()) return  -1; 
     loadAssets();
-
+    initPieces();
     while (game_running)
     {
         inputHandler();
@@ -57,9 +59,6 @@ bool loadAssets() {
     pieces[4] = LoadTexture("assets/puzzle/05.jpg");
     pieces[5] = LoadTexture("assets/puzzle/06.jpg");
 
-    for(int i = 0; i < 6; i++){
-
-    }
     return  true;
 }
 
@@ -77,21 +76,58 @@ void update() {
     DrawTexture(tile, 0, 0, WHITE);
     DrawTexture(player_asset, 10, 145, WHITE);
     DrawTexture(virado, 200, 200, WHITE);
-    quebraCabeca();
-    DrawTexture(pieces[0], 400, 400, WHITE);
+    /*if(CheckCollisionPointRec(mousePos, teste)){
+        DrawRectangleRec(teste, MAGENTA);
+    }else{
+        DrawRectangleRec(teste, YELLOW);
+    }*/
+    puzzle();
+
     EndDrawing();
 
 
 }
 
-void quebraCabeca (){
-    //testa se o botão esquerda está emitindo sinal. caso sim, atualiza posição do quadrado
-    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)){
-        teste.x = GetMouseX() - teste.width/2;
-        teste.y = GetMouseY() - teste.height/2;
+void puzzle (){
+    DrawTexture(mascara, 0, 0, Fade(BLACK, 0.6f));
+    DrawRectangleRec(grid, Fade(WHITE, 0.5f));
+    DrawRectangleLinesEx(grid,2, BLACK);
+    DrawRectangleRec(box, Fade(WHITE, 0.5f));
+    DrawRectangleLinesEx(box, 2, BLACK);
+    checkPieces();
+    checkMouse();
+
+}
+void checkPieces(){
+    int i;
+    //passa por cada um dos elementos do vetor de peças e imprime na tela usando as coordenadas no vetor coords
+    for(i = 0; i < 6; i++){
+        DrawTexture(pieces[i], coords[i].x, coords[i].y, WHITE);
+        DrawRectangleLinesEx(coords[i], 2, BLACK);
+    }
+}
+
+void checkMouse(){
+    int i;
+    //este for serve para mover as peças, atualizando a posição dos retângulos que as representam
+    //com estas coordenadas, ele desenha as peças na tela
+    //PROBLEMA: a função está detectando o mouse passando por cima de outros quadrados e agrupando na mesma coordenada
+    for(i = 0; i < 6; i++){
+        //testa se o botão esquerda está emitindo sinal. caso sim, atualiza posição do retangulo
+        if(IsMouseButtonDown(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(mousePos, coords[i])) {
+            coords[i].x = GetMouseX() - coords[i].width / 2;
+            coords[i].y = GetMouseY() - coords[i].height / 2;
+        }
+            //if(CheckCollisionPointRec(mousePos, coords[i])){
+                //DrawRectangleRec(piecesCoords, MAGENTA);
+                //DrawTexture(pieces[i], coords[i].x, coords[i].y, WHITE);
+            //}else{
+                //DrawRectangleRec(piecesCoords, YELLOW);
+                //DrawTexture(pieces[i], coords[i].x, coords[i].y, WHITE);
+            //}
     }
 
-    //testa para que o quadrado não saia dos limites da janela
+    //testa para que o quadrado/retângulo não saia dos limites da janela
     if(teste.x + teste.width >= GetScreenWidth()){
         teste.x = GetScreenWidth() - teste.width;
     }else if(teste.x < 0){
@@ -102,26 +138,24 @@ void quebraCabeca (){
     }else if(teste.y < 0){
         teste.y = 0;
     }
+}
 
-    if(CheckCollisionPointRec(mousePos, teste)){
-        DrawRectangleRec(teste, MAGENTA);
-    }else{
-        DrawRectangleRec(teste, YELLOW);
+void initPieces (){
+    int i, j = 45;
+
+    for(i = 0; i < 6; i++){
+        coords[i].height = coords[i].width = 130;
+        if(i % 2 == 0){
+            coords[i].x = 460 - 5;
+        }else{
+            coords[i].x = 460 + 5;
+        }
+
+        coords[i].y = j;
+        j = j + 45;
     }
-    DrawTexture(mascara, 0, 0, Fade(BLACK, 0.6f));
-
-    DrawRectangleRec(grid, Fade(WHITE, 0.5f));
-    DrawRectangleLinesEx(grid,2, BLACK);
-    DrawRectangleRec(box, Fade(WHITE, 0.5f));
-    DrawRectangleLinesEx(box, 2, BLACK);
-}
-void checkPieces(Rectangle grid,Texture2D piece[]){
-
 }
 
-bool checkMouseCollision(Texture2D pieces[]){
-    
-}
 void inputHandler() {
     if(WindowShouldClose()) game_running = 0;
 }
